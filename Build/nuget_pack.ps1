5519c5f8df68cdf4aacc10d9b0f8a8d9fea57496
ema <mccoy39082@163.com>
2026-01-26 16:51:15 +0800

    Replace nuget_pack.cmd with PowerShell script
    Removed the batch script for NuGet packaging and added an equivalent PowerShell script (nuget_pack.ps1) to improve cross-platform compatibility and maintainability.

---
+++ Build/nuget_pack.ps1
@@ -0,0 +1,25 @@
+﻿# Change to the script directory
+Set-Location -Path $PSScriptRoot
+
+# RemoteViewing
+Set-Location -Path (Join-Path $PSScriptRoot '..\RemoteViewing')
+dotnet restore
+dotnet build -c Release
+dotnet pack -c Release -o ../Build/
+
+# RemoteViewing.Windows.Forms
+Set-Location -Path (Join-Path $PSScriptRoot '..\RemoteViewing.Windows.Forms')
+dotnet restore
+dotnet build -c Release
+dotnet pack -c Release -o ../Build/
+
+# RemoteViewing.WPF
+Set-Location -Path (Join-Path $PSScriptRoot '..\RemoteViewing.WPF')
+dotnet restore
+dotnet build -c Release
+dotnet pack -c Release -o ../Build/
+
+# Return to the script directory
+Set-Location -Path $PSScriptRoot
+
+Pause # Keep the window open
